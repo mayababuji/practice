62325d74edf82fd62246fc7bfeac5414d4526d5d
mayababuji <mayababuji@outlook.com>
2017-04-09 21:12:37 -0700

    Add word count

---
+++ word_count.py
@@ -0,0 +1,15 @@
+#!usr/bin/env python
+
+def word_count(filename):
+    wordcount = {}
+    with open(filename) as f:
+        for word in f.read().split():
+            if wordcount.has_key(word):
+                wordcount[word] += 1
+            else:
+                wordcount[word] = 1
+    print wordcount
+
+if __name__ == "__main__":
+    filename = "wordcount.txt"
+    word_count(filename)
