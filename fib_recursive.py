cebdd4c6ae4de9d1468e84df460950e8e0d240d4
mayababuji <mayababuji@outlook.com>
2019-08-30 08:50:58 -0700

    find nth val of  fibinocci series in recursive way
    Program to find nth value of a fibinocci series in recursive way

---
+++ fib_recursive.py
@@ -0,0 +1,12 @@
+#Program to find nth value of a fibinocci series in recursive way
+
+def Fib(n):
+   if n < 0:
+     print("invalid input")
+   elif n == 1:
+       return 0
+   elif n == 2:
+       return 1
+   else:
+       return fib(n-1) + fib(n-2)
+print(Fib(9))
